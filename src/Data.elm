d9f805097ccc520c9cbc3aa3e4986a2087a1ae74
unknown <wcourtney31415@gmail.com>
2020-06-13 20:59:35 -0400

    Fixed the imports for the Data module.

--- src/Data.elm
+++ src/Data.elm
@@ -1,21 +1,32 @@
-module Data exposing (..)
+module Data exposing
+    ( backgroundPath
+    , gitHub
+    , myEmail
+    , myName
+    , siteSource
+    )
 
 
+myName : String
 myName =
     "Wesley Courtney"
 
 
+backgroundPath : String
 backgroundPath =
     "./Images/background.jpg"
 
 
+myEmail : String
 myEmail =
     "wcourtney31415@gmail.com"
 
 
+gitHub : String
 gitHub =
     "https://github.com/wcourtney31415"
 
 
+siteSource : String
 siteSource =
     "https://github.com/wcourtney31415/My-Website"
