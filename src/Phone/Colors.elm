6cb16e95819af2ebc78ab81bacf5398bac701bf2
unknown <wcourtney31415@gmail.com>
2020-06-14 05:10:36 -0400

    Fixed imports/exposure for the Phone.Colors module.

--- src/Phone/Colors.elm
+++ src/Phone/Colors.elm
@@ -1,48 +1,64 @@
-module Phone.Colors exposing (..)
+module Phone.Colors exposing
+    ( darkerBlue
+    , gradientColor1
+    , gray
+    , grayThirtyFour
+    , lighterBlue
+    , lighterGray
+    , lightestBlue
+    , myNameColor
+    , theBackground
+    , viewHeaderCol
+    )
 
-import BasicColors exposing (..)
-import Element exposing (..)
+import Element exposing (Color, rgb255)
 
 
+theBackground : Color
 theBackground =
     rgb255 194 194 194
 
 
+gradientColor1 : Color
 gradientColor1 =
     rgb255 228 228 228
 
 
+myNameColor : Color
 myNameColor =
     rgb255 52 89 127
 
 
+grayThirtyFour : Color
 grayThirtyFour =
     rgb255 234 234 234
 
 
+gray : Color
 gray =
     rgb255 200 200 200
 
 
+viewHeaderCol : Color
 viewHeaderCol =
     rgb255 101 147 193
 
 
+darkerBlue : Color
 darkerBlue =
     rgb255 65 115 164
 
 
+lighterBlue : Color
 lighterBlue =
     rgb255 102 148 194
 
 
-darkerGray =
-    rgb255 205 205 205
-
-
+lighterGray : Color
 lighterGray =
     rgb255 230 230 230
 
 
+lightestBlue : Color
 lightestBlue =
     rgb255 182 205 226
