391ce9dee74529239fa146d1f17ddf1781dc26be
wcourtney31415 <38410525+wcourtney31415@users.noreply.github.com>
2026-07-11 19:32:07 +0000

    Change page contents depending on selected page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,73 @@
-import { Button } from "@/components/ui/button"
+import { useState } from "react";
+import { Button } from "./components/ui/button"
+import { ButtonGroup } from "./components/ui/button-group"
+import { Card, CardContent } from "./components/ui/card"
 
 export function App() {
+
+  // Create type to manage selected page
+  type ActiveTab = "myself" | "tools" | "projects"
+
+  // Create variable to manage page selection
+  const [activeTab, setActiveTab] = useState<ActiveTab>("myself");
+
+  // Return the selected page
+  const getActivePage = () => {
+    let page;
+    switch (activeTab) {
+      case "myself":
+        page = myself
+        break;
+      case "tools":
+        page = tools
+        break;
+      case "projects":
+        page = projects
+        break;
+      default:
+        page = <div>Unfortunately this page wasn't found.</div>
+    }
+    return page;
+  }
+
+  // Page: Myself
+  const myself: React.JSX.Element = (
+    <Card className="w-full h-full">
+      <CardContent>
+        <h1 className="scroll-m-20 text-4xl  tracking-tight text-balance">Myself</h1>
+        <img src="https://wcourtney31415.github.io/My-Website/Images/me.png"></img>
+      </CardContent>
+    </Card>
+  )
+
+  // Page: Tools
+  const tools: React.JSX.Element = (
+    <Card className="w-full h-full">
+      <CardContent>
+        <div>Tools</div>
+      </CardContent>
+    </Card>
+  )
+
+  // Page: Projects
+  const projects: React.JSX.Element = (
+    <Card className="w-full h-full">
+      <CardContent>
+        <div>Projects</div>
+      </CardContent>
+    </Card>
+  )
+
+  // Main Page
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
+    <div className="flex min-h-svh p-6 w-full">
+      <div className="flex min-w-0 flex-col gap-4 text-sm leading-loose w-full">
+        <ButtonGroup>
+          <Button onClick={_ => setActiveTab("myself")}>Myself</Button>
+          <Button onClick={_ => setActiveTab("tools")}>My Tools</Button>
+          <Button onClick={_ => setActiveTab("projects")}>My Projects</Button>
+        </ButtonGroup>
+        {getActivePage()}
       </div>
     </div>
   )
