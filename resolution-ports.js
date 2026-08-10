3a4cf115f8359c60f3deaca0e7910983f9ff0334
Wes Courtney <wcourtney31415@gmail.com>
2026-08-10 15:54:54 -0400

    Updates

--- resolution-ports.js
+++
@@ -1,7 +0,0 @@
-function getResponsiveWidth() {
-  Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-}
-
-function getResponsiveHeight() {
-  Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-}
